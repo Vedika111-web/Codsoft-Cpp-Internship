0c9b3147d8a58cbadde32885034829a7eb71dd0e
Vedika111-web <vedikatayade149@gmail.com>
2026-03-11 17:02:21 +0530

    Create Calculator_temp.cpp

---
+++ Task1_Number_Guessing_Game/Task2_Simple_Calculator/Calculator_temp.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    cout << "        SIMPLE CALCULATOR" << endl;
+    cout << "---------------------------------" << endl;
+
+    //  input numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Choose operation
+    cout << "\nChoose an operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "\nResult: ";
+
+    switch(operation) {
+        case '+':
+            cout << num1 + num2 << endl;
+            break;
+
+         case '-':
+            cout << num1 - num2 << endl;
+            break;
+
+        case '*':
+            cout << num1 * num2 << endl;
+            break;
+
+        case '/':
+            if(num2 != 0)
+                cout << num1 / num2 << endl;
+            else
+                cout << "Error! Division by zero is not allowed." << endl;
+            break;
+
+        default:
+            cout << "Invalid operation selected." << endl;
+    }
+
+    cout << "\nThanks for using the Simple Calculator!" << endl;
+
+    return 0;
